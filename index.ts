0fc46cd6af6b5b27b03db7196f9e1b69b6bd6ab5
Marsel <arknahon@gmail.com>
2023-12-26 19:13:04 +0300

    Рефакторинг кода

--- index.ts
+++ index.ts
@@ -4,119 +4,131 @@ const inputTime = document.getElementsByClassName('input_time')[0] as HTMLInputE
 const clearButton = document.getElementsByClassName("clear")[0] as HTMLButtonElement
 const addButton = document.getElementsByClassName("add")[0] as HTMLButtonElement
 
-class toDoItem {
-    text: string;
-    time: string;
-
-    constructor(text : string, time : string) {
-        this.text = text,
-        this.time = time
-    }
+type toDoItemType = {
+  text: string,
+  time: string,
 }
 
-let toDos : string | null = localStorage.getItem('active');
-let activeTasks : toDoItem[]  = toDos !== null ? JSON.parse(toDos) : []
+type tasksType = {
+  active: toDoItemType[],
+  inactive: toDoItemType[],
+}
 
-toDos = localStorage.getItem('inactive')
-let inactiveTasks : toDoItem[] = toDos !== null ? JSON.parse(toDos) : []
+const createToDoItem = (text: string, time: string) : toDoItemType => {
+  return {
+    text: text,
+    time: time,
+  };
+}
 
+const tasks: tasksType = {
+  active: [],
+  inactive: [],
+}
 
-render(activeTasks, inactiveTasks)
+if (localStorage.getItem('active') !== null)
+  tasks.active = JSON.parse(localStorage.getItem('active') as string)
+if (localStorage.getItem('inactive') !== null)
+  tasks.inactive = JSON.parse(localStorage.getItem('inactive') as string)
 
 clearButton.onclick = () => {
-    activeTasks = []
-    inactiveTasks = []
-    render(activeTasks, inactiveTasks)
+  tasks.active = []
+  tasks.inactive = []
+  render(tasks.active, tasks.inactive)
 }
 
 addButton.onclick = () => {
-    let text = inputText.value;
-    let time = inputTime.value;
+  const text = inputText.value;
+  const time = inputTime.value;
 
-    if (text.length >= 1 && time.length >= 1)
-        activeTasks.push(new toDoItem(text, time))
-    else
-        alert('Невозможно добавить пустое поле')
+  if (text.length >= 1 && time.length >= 1)
+    tasks.active.push(createToDoItem(text, time))
+  else
+    alert('Невозможно добавить пустое поле')
 
-    render(activeTasks, inactiveTasks)
+  render(tasks.active, tasks.inactive)
 }
 
-function render(active : toDoItem[], inactive : toDoItem[]) {
-    active.sort(compareByTime)
-    root.innerHTML = ''
+const render = (active : toDoItemType[], inactive : toDoItemType[]) => {
+  active.sort(compareByTime)
+  root.innerHTML = ''
 
-    if (active.length !== 0) {
-        let active_header = document.createElement('h2')
-        active_header.innerText = 'Активные'
-        root.appendChild(active_header)
-        active.map(i => {
-            let item = createItem(i)
+  if (active.length !== 0) {
+    const activeHeader = document.createElement('h2')
+    activeHeader.innerText = 'Активные'
+    root.appendChild(activeHeader)
+    active.map(i => {
+      const item = createItem(i)
 
-            item.addEventListener('click', (event) => {
-                active.splice(active.indexOf(i), 1)
-                inactive.push(i)
-                render(active, inactive)
-                event.stopPropagation()
-            })
+      item.addEventListener('click', (event) => {
+        active.splice(active.indexOf(i), 1)
+        inactive.push(i)
+        render(active, inactive)
+        event.stopPropagation()
+      })
 
-            let bucket = item.querySelector('img') as HTMLElement
-            bucket.addEventListener('click', (event) => {
-                active.splice(active.indexOf(i), 1)
-                render(active, inactive)
-                event.stopPropagation()
-            })
-        })
-    }
-
-    if (inactive.length !== 0) {
-        let inactive_header = document.createElement('h2')
-        inactive_header.innerText = 'Завершенные'
-        root.appendChild(inactive_header)
-        inactive.map(j => {
-            let item = createItem(j)
-            item.className += ' item_succses'
-
-            let bucket = item.querySelector('img') as HTMLElement
-            bucket.addEventListener('click', (event) => {
-                inactive.splice(inactive.indexOf(j), 1)
-                render(active, inactive)
-                event.stopPropagation()
-            })
-        })
-    }
-
-    localStorage.setItem('active', JSON.stringify(active))
-    localStorage.setItem('inactive', JSON.stringify(inactive))
-}
-
-function createItem(toDo : toDoItem) {
-    let item = document.createElement('div')
-    let time = document.createElement('p')
-    let text = document.createElement('p')
-    let delButton = document.createElement('button')
-    let delIcon = document.createElement('img')
-
-    time.textContent = toDo.time
-    text.textContent = toDo.text
-
-    delIcon.src = './assets/ic_delete.svg'
-    delIcon.alt = 'Удалить'
-
-    delButton.appendChild(delIcon)
-
-    item.className = 'item'
-    item.appendChild(time)
-    item.appendChild(text)
-    item.appendChild(delButton)
-    root.appendChild(item)
-    return item
-}
-
-function compareByTime(a:toDoItem, b:toDoItem) {
-    const timeA: Date = new Date(`2000-01-01T${a.time}`);
-    const timeB: Date = new Date(`2000-01-01T${b.time}`);
-    if (timeA < timeB) return -1;
-    if (timeA > timeB) return +1;
-
-    return 0; // dates are equal
+      const bucket = item.querySelector('img') as HTMLElement
+      bucket.addEventListener('click', (event) => {
+        active.splice(active.indexOf(i), 1)
+        render(active, inactive)
+        event.stopPropagation()
+      })
+    })
   }
+
+  if (inactive.length !== 0) {
+    const inactiveHeader = document.createElement('h2')
+    inactiveHeader.innerText = 'Завершенные'
+    root.appendChild(inactiveHeader)
+    inactive.map(j => {
+      const item = createItem(j)
+      item.className += ' item_succses'
+
+      const bucket = item.querySelector('img') as HTMLElement
+      bucket.addEventListener('click', (event) => {
+        inactive.splice(inactive.indexOf(j), 1)
+        render(active, inactive)
+        event.stopPropagation()
+      })
+    })
+  }
+
+  localStorage.setItem('active', JSON.stringify(active))
+  localStorage.setItem('inactive', JSON.stringify(inactive))
+}
+
+const createItem = (toDo : toDoItemType) => {
+  const item = document.createElement('div')
+  const time = document.createElement('p')
+  const text = document.createElement('p')
+  const delButton = document.createElement('button')
+  const delIcon = document.createElement('img')
+
+  time.textContent = toDo.time
+  text.textContent = toDo.text
+
+  delIcon.src = './assets/ic_delete.svg'
+  delIcon.alt = 'Удалить'
+
+  delButton.appendChild(delIcon)
+
+  item.className = 'item'
+  item.appendChild(time)
+  item.appendChild(text)
+  item.appendChild(delButton)
+  root.appendChild(item)
+  return item
+}
+
+const compareByTime = (a: toDoItemType, b: toDoItemType) => {
+  const timeA: Date = new Date(`2000-01-01T${a.time}`);
+  const timeB: Date = new Date(`2000-01-01T${b.time}`);
+  if (timeA < timeB)
+    return -1;
+  if (timeA > timeB)
+    return +1;
+
+  return 0;
+}
+
+render(tasks.active, tasks.inactive)
